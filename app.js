d1012a6a5477456dc0c2098d05fdb3d2ea0f937c
Emanuel Cortez <emanuelcortez32@gmail.com>
2023-12-09 01:42:22 -0300

    refactor log

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ const helmet = require('helmet');
 const {name, version} = require('./package.json');
 const nsfwRouter = require('./src/routes/nsfwRoute');
 
+const BASE_PATH = "/nsfw";
 const PORT = 8080;
 const app = express();
 
 //Middlewares
 app.use(helmet());
-app.use(actuator({basePath: "/nsfw"}));
-app.use(morgan('dev'));
+app.use(actuator({basePath: BASE_PATH}));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 
 //Routes
-app.use('/nsfw', nsfwRouter);
+app.use(BASE_PATH, nsfwRouter);
 
 app.listen(PORT, () => {
     console.log(`${name} ${version} is running on port: ${PORT}!!`);
