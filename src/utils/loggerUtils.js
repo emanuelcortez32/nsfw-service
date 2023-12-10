edb475cac81bbe4d1fba6eb49ec770fcbb410b1d
Emanuel Cortez <emanuelcortez32@gmail.com>
2023-12-09 23:53:45 -0300

    log

--- src/utils/loggerUtils.js
+++ src/utils/loggerUtils.js
@@ -1,10 +1,4 @@
 const morgan = require('morgan');
-const os = require('os');
-
-
-morgan.token('hostname', function getHostname() {
-    return os.hostname();
-});
 
 morgan.token('pid', function getPid() {
     return process.pid;
@@ -16,16 +10,15 @@ morgan.token('x-trace-id', function getTraceId(req) {
 
 const jsonFormat = (tokens, req, res) => {
     return JSON.stringify({
-        'remote-address': tokens['remote-addr'](req, res),
         'time': tokens['date'](req, res, 'iso'),
+        'remote-address': tokens['remote-addr'](req, res),
+        'x-trace-id': tokens['x-trace-id'](req, res),
         'method': tokens['method'](req, res),
         'url': tokens['url'](req, res),
         'http-version': tokens['http-version'](req, res),
         'status-code': tokens['status'](req, res),
         'content-length': tokens['res'](req, res, 'content-length'),
-        'referrer': tokens['referrer'](req, res),
         'user-agent': tokens['user-agent'](req, res),
-        'hostname': tokens['hostname'](req, res),
         'pid': tokens['pid'](req, res)
     });
 }
